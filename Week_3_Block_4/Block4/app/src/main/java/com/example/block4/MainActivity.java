54b85b95bb13f146843e62d37656012442e892bc
whyitebeard <jlasia@student.apc.edu.ph>
2019-08-08 08:55:25 +0800

    Week 3 Block 4

---
+++ Week_3_Block_4/Block4/app/src/main/java/com/example/block4/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.block4;
+
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer pianoMusic;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pianoMusic = MediaPlayer.create(this, R.raw.piano);
+        Switch switch_looping = findViewById(R.id.switch_looping);
+        switch_looping.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                pianoMusic.setLooping(isChecked);
+            }
+        });
+    }
+
+    public void playMusic (View v){
+        pianoMusic.start();
+    }
+    public void pauseMusic(View v){
+        if(pianoMusic.isPlaying())
+            pianoMusic.pause();
+
+    }
+}
